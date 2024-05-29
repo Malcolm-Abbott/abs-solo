bc876f24f520981043da224ec8d06de6e5705fde
Malcolm Abbott <mjacoding@gmail.com>
2024-05-29 21:12:05 +0000

    responsive nav bar: progress into desktop version, nav links added and spaced accordingly

--- client/src/Components/NavBar.tsx
+++ client/src/Components/NavBar.tsx
@@ -1,26 +1,47 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, Link } from 'react-router-dom';
 import { FaBars } from 'react-icons/fa';
 
 export function NavBar() {
   return (
-    <div className="container py-4 px-6">
-      <nav>
-        <ul>
-          <li>
-            <div className="flex justify-between">
-              <div className="w-2/3">
-                <div className="w-full">
-                  <img src="absi-logo-full.png" />
-                </div>
-              </div>
-              <div className="grid place-items-center">
-                <FaBars className="text-3xl" />
+    <>
+      <div className="container py-4 px-6">
+        <header>
+          <div className="flex justify-between">
+            <div className="w-2/3 xl:w-1/3">
+              <div className="w-full">
+                <img src="absi-logo-full.png" />
               </div>
             </div>
-          </li>
-        </ul>
-      </nav>
-      <Outlet />
-    </div>
+            <div className="grid place-items-center xl:hidden">
+              <FaBars className="text-3xl cursor-pointer" />
+            </div>
+            <div className="hidden xl:flex w-2/3">
+              <div className="grid place-items-center basis-1/3">
+                <Link
+                  to="/"
+                  className="h-3/4 flex items-end text-lg font-bold pb-1">
+                  Services
+                </Link>
+              </div>
+              <div className="grid place-items-center basis-1/3">
+                <Link
+                  to="/"
+                  className="h-3/4 flex items-end text-lg font-bold pb-1">
+                  Customer Portal
+                </Link>
+              </div>
+              <div className="grid place-items-center basis-1/3 ring-2 ring-red-700">
+                <Link
+                  to="/"
+                  className="h-3/4 flex items-end text-lg font-bold pb-1">
+                  Customer Portal
+                </Link>
+              </div>
+            </div>
+          </div>
+        </header>
+        <Outlet />
+      </div>
+    </>
   );
 }
