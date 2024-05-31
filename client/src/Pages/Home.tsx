60f8e8abc0ad8ce112478dfe550c41210bbdc75f
Malcolm Abbott <mjacoding@gmail.com>
2024-05-31 21:42:39 +0000

    Responsive design nearly complete

--- client/src/Pages/Home.tsx
+++ client/src/Pages/Home.tsx
@@ -1,3 +1,6 @@
+import { Booking } from '../Components/Booking';
+import { About } from '../Components/About';
+
 export function Home() {
   return (
     <div>
@@ -22,31 +25,22 @@ export function Home() {
             Our engineering and maintenance team bring 40+ years of experience
           </h3>
         </div>
-        <div className="grid place-items-center gap-6">
-          <h2 className="font-semibold">Proud to be employee-owned.</h2>
-          <h3 className="font-semibold text-center">
-            Serving the Washington DC, Maryland, and Virginia area since 1997
-          </h3>
-          <button className="font-semibold bg-blue-950 py-3 px-6 rounded-lg shadow-md">
-            Book a Consultation
-          </button>
-        </div>
-        <div className="grid gap-6 mt-2">
-          <div>
-            From 100 year old boiler units to modern digital systems, Advanced
-            Building Services has over 25 years of experience of customizing and
-            providing excellent service ensuring we meet your needs. Our team is
-            equipped to provide full time building engineers to semi-annual
-            maintenance including HVAC, plumbing, electrical and even handyman
-            services. A proactive and consistent approach with the right
-            maintenance can add years to the life expectancy of your equipment,
-            maximizing the benefits of your investment and avoiding costly
-            surprises.
+        <div className="xl:hidden">
+          <div className="grid place-items-center gap-6">
+            <Booking />
           </div>
-          <div>
-            We are just a phone call away to resolve any issue with our 24/7
-            emergency service. Our highly qualified and licensed team provides
-            exceptional quality for your essential building and HVAC needs.
+          <div className="grid gap-6 mt-6">
+            <About />
+          </div>
+        </div>
+        <div className="hidden xl:flex">
+          <div className="grid gap-6 mt-6 basis-1/2">
+            <About />
+          </div>
+          <div className="flex basis-1/2">
+            <div className="m-auto">
+              <Booking />
+            </div>
           </div>
         </div>
       </div>
