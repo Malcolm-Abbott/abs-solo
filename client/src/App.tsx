921f4cbd0fbce56b9a9e68fcd3e1dc897e138e8f
Malcolm Abbott <mjacoding@gmail.com>
2024-06-25 19:59:26 +0000

    updated

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import { NavBar } from './Components/NavBar';
-import { Home } from './Pages/Home';
+import { NewHome } from './Pages/NewHome';
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<NavBar />}>
-        <Route index element={<Home />} />
+        <Route index element={<NewHome />} />
       </Route>
     </Routes>
   );
